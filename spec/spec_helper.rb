93eb0969892bca116018ffc0ea4fc89bd0ab1645
upcFrost <upcfrost@gmail.com>
2016-05-13 16:56:24 +0300

    Added test coverage report generation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
+ChefSpec::Coverage.start! { add_filter 'gliderlabs_registrator' }
+
 RSpec.configure do |config|
   # Make the output look nice
   config.color = true
